35d779c99336e71c8723533e78aa5fd93c7fd3e5
Илья <pitermar@student.s21-school.ru>
2024-05-27 12:30:55 +0300

    Maze

--- internal/model/cave_test.go
+++ internal/model/cave_test.go
@@ -9,64 +9,62 @@ import (
 	colorT "github.com/fatih/color"
 )
 
-func TestCave(t *testing.T) {
-	const imageSize = 500
+const imageSize = 500
 
-	t.Run("TestLoad&Save", func(t *testing.T) {
-		cave := model.NewCave()
-		result := model.NewCave()
+func TestLoadSaveCave(t *testing.T) {
+	cave := model.NewCave()
+	result := model.NewCave()
 
-		cave.Load("../../txt/cave_10.txt")
-		cave.Save("Load&SaveCave.txt")
-		result.Load("Load&SaveCave.txt")
+	cave.Load("../../txt/cave_10.txt")
+	cave.Save("Load&SaveCave.txt")
+	result.Load("Load&SaveCave.txt")
 
-		if !reflect.DeepEqual(result, cave) {
-			errorMsg := colorT.MagentaString(`loadCave: "%v", safeCave: "%v"`, cave, result)
-			t.Errorf(errorMsg)
+	if !reflect.DeepEqual(result, cave) {
+		errorMsg := colorT.MagentaString(`loadCave: "%v", safeCave: "%v"`, cave, result)
+		t.Errorf(errorMsg)
+	}
+
+	cave.Clear()
+	if !reflect.DeepEqual(true, cave.IsEmpty()) {
+		errorMsg := colorT.MagentaString(`result: "false", need: "true"`)
+		t.Errorf(errorMsg)
+	}
+}
+
+func TestGenerateUpdateCave(t *testing.T) {
+	rows, cols := 10, 10
+	cave := model.NewCave()
+
+	cave.GenerateCave(rows, cols, 100, 50)
+	if !reflect.DeepEqual(rows, cave.GetRows()) {
+		errorMsg := colorT.MagentaString(`rows: "%v", need: "%v"`, cave.GetRows(), rows)
+		t.Errorf(errorMsg)
+	}
+
+	if !reflect.DeepEqual(rows, cave.GetCols()) {
+		errorMsg := colorT.MagentaString(`cols: "%v", need: "%v"`, cave.GetCols(), cols)
+		t.Errorf(errorMsg)
+	}
+
+	cave.Load("../../txt/cave_10.txt")
+	for {
+		res, _ := cave.UpdateCave(4, 3)
+		if !res {
+			break
 		}
+	}
+	result := model.NewCave()
+	result.Load("../../txt/result.txt")
 
-		cave.Clear()
-		if !reflect.DeepEqual(true, cave.IsEmpty()) {
-			errorMsg := colorT.MagentaString(`result: "false", need: "true"`)
-			t.Errorf(errorMsg)
-		}
-	})
+	if !reflect.DeepEqual(cave, result) {
+		errorMsg := colorT.MagentaString(`result: "%v", need: "%v"`, cave, result)
+		t.Errorf(errorMsg)
+	}
 
-	t.Run("TestGenerate&Update", func(t *testing.T) {
-		rows, cols := 10, 10
-		cave := model.NewCave()
+	screen := model.CreatePixelMatrix(imageSize, imageSize, 255, 255, 255)
+	cave.ScaleToPixelMatrix(screen, imageSize, imageSize, color.RGBA{0, 0, 0, 255})
 
-		cave.GenerateCave(rows, cols, 100, 50)
-		if !reflect.DeepEqual(rows, cave.GetRows()) {
-			errorMsg := colorT.MagentaString(`rows: "%v", need: "%v"`, cave.GetRows(), rows)
-			t.Errorf(errorMsg)
-		}
-
-		if !reflect.DeepEqual(rows, cave.GetCols()) {
-			errorMsg := colorT.MagentaString(`cols: "%v", need: "%v"`, cave.GetCols(), cols)
-			t.Errorf(errorMsg)
-		}
-
-		cave.Load("../../txt/cave_10.txt")
-		for {
-			res, _ := cave.UpdateCave(4, 3)
-			if !res {
-				break
-			}
-		}
-		result := model.NewCave()
-		result.Load("../../txt/result.txt")
-
-		if !reflect.DeepEqual(cave, result) {
-			errorMsg := colorT.MagentaString(`result: "%v", need: "%v"`, cave, result)
-			t.Errorf(errorMsg)
-		}
-
-		screen := model.CreatePixelMatrix(imageSize, imageSize, 255, 255, 255)
-		cave.ScaleToPixelMatrix(screen, imageSize, imageSize, color.RGBA{0, 0, 0, 255})
-
-		model.CreateImage(screen, "testCave.png")
-	})
+	model.CreateImage(screen, "testCave.png")
 }
 
 func TestErrorsCave(t *testing.T) {
